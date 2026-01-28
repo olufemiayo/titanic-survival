4869684190e4a437a657c93741a299088c68704e
Olufemi Makinde <130155355+olufemiayo@users.noreply.github.com>
2026-01-28 14:55:13 +0100

    Update main.py
    port number change

--- titanic/app/main.py
+++ titanic/app/main.py
@@ -31,7 +31,8 @@ def predict_survival(passenger: Passenger):
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+
 
 
 
